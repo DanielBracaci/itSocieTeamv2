a0ede248d331b6195b0c87c17123b5de2838aafa
Bracaci Daniel <dbracaci@gmail.com>
2021-05-27 23:30:49 +0300

    first modify

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
